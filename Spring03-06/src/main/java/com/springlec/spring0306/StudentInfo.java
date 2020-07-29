7c92b92220ee00ec1deefb16ed6597bbaff4e260
zzeung615 <zzeung615@gmail.com>
2020-07-29 16:46:05 +0900

    확인

--- Spring03-06/src/main/java/com/springlec/spring0306/StudentInfo.java
+++ Spring03-06/src/main/java/com/springlec/spring0306/StudentInfo.java
@@ -2,6 +2,22 @@ package com.springlec.spring0306;
 
 public class StudentInfo {
 	
+	private Student student;
+
+	public StudentInfo() {
+		
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	
+	
+	
 	
 
 }
